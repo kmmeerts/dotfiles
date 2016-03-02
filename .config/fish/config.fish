f37b01dd0d26353aa8afa611993c5cad63846682
Kasper Meerts <kaspermeerts@gmail.com>
2016-03-02 23:21:42 +0100

    Cleaned up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,14 +4,18 @@ alias gdb="gdb -q"
 alias bc="bc -lq"
 alias gcc="gcc -Wall -Wextra"
 alias hd="hexdump -C"
+alias yolo="yaourt -Syua"
+alias temp="cat /sys/class/thermal/thermal_zone*/temp"
 
 set -e fish_greeting
 function fish_greeting -d "Greet the user"
-	test -z (git --git-dir=$HOME/.git --work-tree=$HOME status --porcelain)
+	git --git-dir=$HOME/.git --work-tree=$HOME diff-index --quiet HEAD --
 	or echo "dotfiles git repository needs updating"
 end
 
+# TODO __fish_git_prompt
 function fish_prompt -d "Write out the prompt"
-	and printf '%s%s%s> ' (set_color green) (prompt_pwd) (set_color normal)
-	or  printf '%s%s%s> ' (set_color red) (prompt_pwd) (set_color normal)
+	and set -l pwd_color green
+	or  set -l pwd_color red
+	printf '%s%s%s> ' (set_color $pwd_color) (prompt_pwd) (set_color normal)
 end
